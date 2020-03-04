fede4a89deb31f9aa27b9e82ea17cf7380c79330
YuChenxin <ycx_mhobfy@163.com>
2020-03-04 20:52:33 +0800

    求质数因子

---
+++ 第二次作业/质数/质数/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace 质数
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("请输入一个数字：");
+            int num=int.Parse(Console.ReadLine());
+            Console.WriteLine("质数因子为：");
+            
+            if(num<=1)
+            {
+                Console.WriteLine("质数因子不存在，请输入一个大于等于2的数字。");
+                return;
+            }
+           
+            for (int i = 2; i <= num;i++)
+            {
+                for(; num%i==0;num/=i)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
