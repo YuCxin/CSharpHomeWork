9145e3eb4344c83699edf56556501f1c229a5f0c
YuChenxin <ycx_mhobfy@163.com>
2020-03-05 18:17:25 +0800

    数组

---
+++ 第二次作业/数组/数组/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace 数组
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] array = new int[] { 1, 2, 4, 3, 0, -1, 34, 545, 2, 34 };
+
+            int min = Program.Min(array);
+            int max = Program.Max(array);
+            double? average = Program.Average(array);
+            Console.WriteLine("Min:" + min);
+            Console.WriteLine("Max:" + max);
+            Console.WriteLine("Average:" + average);
+        }
+
+        // 最小值        
+        public static int Min(int[] array)
+        {
+            if (array == null) throw new Exception("数组空异常");
+            int value = 0;
+            bool hasValue = false;
+            foreach (int x in array)
+            {
+                if (hasValue)
+                {
+                    if (x < value) value = x;
+                }
+                else
+                {
+                    value = x;
+                    hasValue = true;
+                }
+            }
+            if (hasValue) return value;
+            throw new Exception("没找到");
+        }
+
+        // 最大值        
+        public static int Max(int[] array)
+        {
+            if (array == null) throw new Exception("数组空异常");
+            int value = 0;
+            bool hasValue = false;
+            foreach (int x in array)
+            {
+                if (hasValue)
+                {
+                    if (x > value)
+                        value = x;
+                }
+                else
+                {
+                    value = x;
+                    hasValue = true;
+                }
+            }
+            if (hasValue) return value;
+            throw new Exception("没找到");
+        }
+
+        // 平均值         
+        public static double? Average(int[] array)
+        {
+            if (array == null) throw new Exception("数组空异常");
+            long sum = 0;
+            long count = 0;
+            checked
+            {
+                foreach (int? v in array)
+                {
+                    if (v != null)
+                    {
+                        int a = v.GetValueOrDefault();
+                        sum += v.GetValueOrDefault();
+                        count++;
+                    }
+                }
+            }
+            if (count > 0) return (double)sum / count;
+            return null;
+        }
+    }
+}
