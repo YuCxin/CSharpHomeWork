070e55d17ff9e1bbfc53705627942a69df8ecb84
YuChenxin <ycx_mhobfy@163.com>
2020-05-29 15:18:03 +0800

    order

---
+++ 第5次作业/ordermanage/OrderItem.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+namespace ordermanage
+{
+    //订单明细项
+    public class OrderItem
+    {
+        public uint ItemId { get; set; }//商品id
+        public string ItemName { get; set; }//商品名
+        public uint ItemAmount { get; set; }//商品数量
+        public double ItemPrice { get; set; }//商品单价
+
+        public OrderItem(uint ItemId, string ItemName, uint ItemAmount, double ItemPrice)
+        {
+            this.ItemId = ItemId;
+            this.ItemName = ItemName;
+            this.ItemAmount = ItemAmount;
+            this.ItemPrice = ItemPrice;
+        }
+
+        public double ItemTotalPrice
+        {
+            get => ItemPrice * ItemAmount;
+        }
+
+        //商品信息
+        public override string ToString()
+        {
+            return $"[No.:{ItemId},goods:{ItemName},quantity:{ItemAmount},totalPrice:{ItemTotalPrice}]";
+        }
+
+        public override bool Equals(object obj)
+        {
+            var item = obj as OrderItem;
+            return item != null &&
+                   ItemName == item.ItemName &&
+                   ItemId == item.ItemId;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(ItemName, ItemId);
+        }
+    }
+
+
+}
---
+++ 第5次作业/ordermanage/OrderService.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+namespace ordermanage
+{    //订单服务 增删改查
+    public class OrderService
+    {
+
+        private List<Order> orders;
+
+
+        public OrderService()
+        {
+            orders = new List<Order>();
+        }
+
+        public List<Order> Orders
+        {
+            get => orders;
+        }
+
+        public Order GetOrder(string id)
+        {
+            return orders.Where(o => o.OrderId == id).FirstOrDefault();
+        }
+
+        public void AddOrder(Order order)
+        {
+            if (orders.Contains(order))
+                throw new ApplicationException($"Add Order Error: Order with id {order.OrderId} already exists!");
+            orders.Add(order);
+        }
+
+        public void RemoveOrder(string Customer)
+        {
+            Order order = GetOrder(Customer);
+            if (order != null)
+            {
+                orders.Remove(order);
+            }
+        }
+
+        public List<Order> QueryOrdersByGoodsName(string goodsName)
+        {
+            var query = orders
+                    .Where(order => order.Items.Exists(item => item.ItemName == goodsName))
+                    .OrderBy(o => o.TotalPrice);
+            return query.ToList();
+        }
+
+        public List<Order> QueryOrdersByCustomerName(string customerName)
+        {
+            return orders
+                .Where(order => order.Customer == customerName)
+                .OrderBy(o => o.TotalPrice)
+                .ToList();
+        }
+
+        public void UpdateOrder(Order newOrder)
+        {
+            Order oldOrder = GetOrder(newOrder.OrderId);
+            if (oldOrder == null)
+                throw new ApplicationException($"Update Error：the order with id {newOrder.OrderId} does not exist!");
+            orders.Remove(oldOrder);
+            orders.Add(newOrder);
+        }
+
+        public void Sort()
+        {
+            orders.Sort();
+        }
+
+        public void Sort(Func<Order, Order, int> func)
+        {
+            Orders.Sort((o1, o2) => func(o1, o2));
+        }      
+   
+    }
+}
+
