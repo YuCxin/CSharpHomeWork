98000451772c02d820ad450079e2a8b239924fda
YuChenxin <ycx_mhobfy@163.com>
2020-05-30 13:13:15 +0800

    ordermanage

---
+++ 第6次作业/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Xml.Serialization;
+
+namespace ordermanage
+{
+    class Program
+    {
+        public static void Main()
+        {
+            try
+            {
+                OrderItem pen = new OrderItem(001, "pen", 5, 2.5);
+                OrderItem eraser = new OrderItem(002, "eraser",2, 1);
+                OrderItem ruler = new OrderItem(003, "ruler", 1, 5);
+
+                Order order1 = new Order("Xindy", new List<OrderItem> { pen, eraser, ruler});
+                Order order2 = new Order("Rosa", new List<OrderItem> { pen,ruler });
+
+                OrderService os = new OrderService();
+                os.AddOrder(order1);
+                os.AddOrder(order2);
+                os.Export(@"./orders.xml");
+
+                Console.WriteLine("GetAllOrders");
+                List<Order> orders = os.Orders;
+                orders.ForEach(o => Console.WriteLine(o));
+                Console.WriteLine("");
+
+                os.Sort((o1, o2) => o1.TotalPrice.CompareTo(o2.TotalPrice));
+                Console.WriteLine("GetAllOrders After sort");
+                orders = os.Orders;
+                orders.ForEach(o => Console.WriteLine(o));
+                Console.WriteLine("");
+
+                Console.WriteLine("GetOrdersByCustomerName:'Customer2'");
+                orders = os.QueryOrdersByCustomerName("Customer2");
+                orders.ForEach(o => Console.WriteLine(o));
+                Console.WriteLine("");
+
+                Console.WriteLine("GetOrdersByGoodsName:'apple'");
+                orders = os.QueryOrdersByGoodsName("apple");
+                orders.ForEach(o => Console.WriteLine(o));
+                Console.WriteLine("");
+
+                Console.WriteLine("Remove order(Customer:Rosa) and qurey all");
+                os.RemoveOrder("Rosa");
+                orders.ForEach(o => Console.WriteLine(o));
+                Console.WriteLine("");
+
+                Console.WriteLine("Import from ./orders.xml");
+                OrderService os2 = new OrderService();
+                os2.Import("./orders.xml");
+                os2.Orders.ForEach(order => Console.WriteLine(order));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+            }
+
+        }
+        
+    }
+}
+
